434c5f8c19885294b2ca47addda655f7a5915f2d
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-11-23 00:39:41 -0800

    add to color tasklist UI

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,15 +6,22 @@ const TaskList = () => {
   console.log(stateTasks);
 
   return (
-    <div>
-      <h1>TaskList</h1>
-      <ul>
-        {stateTasks.map((state) => (
-          <li key={state.id}>{state.title}</li>
+    <div className="w-4/6">
+      <div className="grid grid-cols-3 gap-3">
+        {stateTasks.map((task) => (
+          <div className="bg-gray-200 p-4 rounded-md" key={task.id}>
+            <header className="flex justify-between">
+              <h3 className="text-lg font-bold">{task.title}</h3>
+            </header>
+            <p>{task.description}</p>
+            <p className="text-xs text-slate-400">{task.id}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
+
+// description
 
 export default TaskList;
